751acfdee22abdf3cff4fba301e02f4705cc073a
lucky18042000 <rohitsingh182000@gmail.com>
2023-01-12 19:21:16 +0530

    added the api fetch request

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import img from '../assets/beata-biskupicova-81IbsV021f0-unsplash.jpg'
 function Cards(product) {
-    
-    console.log(product)
+
   return (
     <div className='cards cursor-pointer'>
       <div className='cards-image'>
-        <img className='cards-image-img' src={product.goods_img} alt="" srcset="" />
+        <img className='cards-image-img' src={product.goods_img} alt="" srcSet="" />
       </div>
       <h2 className='cards-name'>{product.goods_name}</h2>
       <p className='cards-price'>{product.retailPrice.amountWithSymbol}</p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
-import About from './About'
+import React,{useState,useEffect} from "react";
+import Hero from "../Components/Hero";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import About from "./About";
 import { Routes, Route } from "react-router-dom";
-import Cards from '../Components/Cards'
-import data from './data'
+import Cards from "../Components/Cards";
+import data from "./data";
+
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3f7fb41c9amsh15ce7cb2af9827bp1e5041jsn2ddf53c2094b',
+    'X-RapidAPI-Host': 'unofficial-shein.p.rapidapi.com',
+  },
+};
 
 function Home() {
-
-  const cards= data.map((item)=>{
-    return<Cards key={data.id} {...item}/>
-  })
-
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://unofficial-shein.p.rapidapi.com/products/list?cat_id=1980&adp=10170797&language=en&country=US&currency=USD&sort=7&limit=20&page=1',
+      options
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        setCards(response.info.products);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  
+  const cardlist = cards.map((item) => {
+    return <Cards key={item.goods_id} {...item} />;
+  });
+console.log(cards)
   return (
-    <div className='home'>
-      <Navbar active={(false)}/>
-      <Hero/>
-      <div className='cards-main justify-center'>
-      {cards}
-      </div>
-      <Footer/>
+    <div className="home">
+      <Navbar active={false} />
+      <Hero />
+      <div className="cards-main justify-center">{cardlist}</div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
