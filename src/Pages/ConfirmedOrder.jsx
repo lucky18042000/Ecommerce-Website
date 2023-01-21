b9c22003e582728700886e8b60fbd055bec403b9
lucky18042000 <rohitsingh182000@gmail.com>
2023-01-21 09:19:30 +0530

    scroll to top on click

--- src/Pages/ConfirmedOrder.jsx
+++ src/Pages/ConfirmedOrder.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import svg from '../assets/success.svg'
+import { useLocation } from "react-router-dom";
 
 function ConfirmedOrder() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname === '/confirmorder']);
   return (
     <div className="confiremedorder">
       <Navbar/>
